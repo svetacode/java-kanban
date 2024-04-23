b19e205f044669a562266b4b368df9fef33611eb
svetacode <melnichu-svetlana@yandex.ru>
2024-04-23 20:31:25 +0300

    Spring 7 - финальное задание

--- src/ru/sveta/kanban/task/Task.java
+++ src/ru/sveta/kanban/task/Task.java
@@ -3,89 +3,93 @@ package ru.sveta.kanban.task;
 import java.util.Objects;
 
 public class Task {
-    protected int id;
-    protected String title;
-    protected String description;
-    protected TaskStatus status;
 
-    public Task(String title, String description, TaskStatus status) {
-        this.title = title;
-        this.description = description;
-        this.status = status;
+  protected int id;
+  protected String title;
+  protected String description;
+  protected TaskStatus status;
+
+  public Task(String title, String description, TaskStatus status) {
+    this.title = title;
+    this.description = description;
+    this.status = status;
+  }
+
+  private Task(int id, String title, String description, TaskStatus status) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+  }
+
+  public static Task fromCsvFormat(String[] csv) {
+    return new Task(
+        Integer.parseInt(csv[0]),
+        csv[2],
+        csv[4],
+        TaskStatus.valueOf(csv[3])
+    );
+  }
+
+  public TaskType getTaskType() {
+    return TaskType.TASK;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public TaskStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(TaskStatus status) {
+    this.status = status;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
-
-    private Task(int id, String title, String description, TaskStatus status) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.status = status;
+    if (o == null || getClass() != o.getClass()) {
+      return false;
     }
+    Task task = (Task) o;
+    return id == task.id;
+  }
 
-    private Task() {}
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
 
-    public TaskType getTaskType() {
-        return TaskType.TASK;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public TaskStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(TaskStatus status) {
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Task task = (Task) o;
-        return id == task.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    /**
-     * Преобразуем задачу в формат CSV
-     * @return CSV-представление задачи
-     */
-    public String toCsvFormat(){
-        return String.format("%s,%s,%s,%s,%s\n", id, getTaskType(), title, status, description);
-    }
-
-    public static Task fromCsvFormat(String[] csv){
-        return new Task(
-            Integer.parseInt(csv[0]),
-            csv[2],
-            csv[4],
-            TaskStatus.valueOf(csv[3])
-        );
-    }
+  /**
+   * Преобразуем задачу в формат CSV
+   *
+   * @return CSV-представление задачи
+   */
+  public String toCsvFormat() {
+    return String.format("%s,%s,%s,%s,%s\n", id, getTaskType(), title, status, description);
+  }
 }
